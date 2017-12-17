85d7417ecb70f33a71c9058d3f1ee183ff2cf6c4
gbourdon <34389449+gbourdon@users.noreply.github.com>
2017-12-16 19:56:38 -0700

    Started createing basic logic

--- generator.rb
+++ generator.rb
@@ -1,3 +1,7 @@
+def randBool
+    rand(0 .. 1) != 0
+end
+
 def readlines(file) # How to read a file's lines?
     result = [] # Sets up array "Result"
     File.open(file).each do |line| # Opens the file
@@ -8,3 +12,8 @@ end
 
 age_range = (18 .. 100) # Set this to whatever age range you want
 gender = nil # Set this to a specific gender that you want your person to be
+
+age = rand(age_range)
+gender ||= randBool() ? "Guy" : "Girl"
+
+puts "Person is #{age} and a #{gender}"
